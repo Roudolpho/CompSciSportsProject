fd757215db41188f2499c8e3bd01956a5df28f63
lcrisco <37844342+lcrisco@users.noreply.github.com>
2018-03-28 13:18:04 -0400

    sajd
    ds

--- Volleyball.java
+++ Volleyball.java
@@ -5,28 +5,60 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Volleyball extends Sport{
-    // instance variables - replace the example below with your own
-    private int x;
+import java.util.*;
+public class Volleyball extends Sport
+{
 
-    /**
-     * Constructor for objects of class Volleyball
-     */
+    private int blocks, kills, aces, sets, hits, digs;
+    private int netViolation, footFault, injuries;
+    private int height, reach, verticle, blockJump, approachJump;
+
+    Scanner s = new Scanner(System.in);
+
     public Volleyball()
     {
-        // initialise instance variables
-        x = 0;
+        System.out.println("How tall are they (inches)?");
+        height = s.nextInt();
+        System.out.println("What is their reach?");
+        reach = s.nextInt(); 
+        System.out.println("What is their verticle?");
+        verticle = s.nextInt(); 
+        System.out.println("What is their block jump?");
+        blockJump = s.nextInt();
+        System.out.println("What is their approch jump?");
+        approachJump = s.nextInt();
+      
+       
+        
+        System.out.println("How many block did they have?");
+        blocks = s.nextInt();
+        System.out.println("How many games did they have?");
+        hits  = s.nextInt();
+        System.out.println("How many kills did they have?");
+        kills = s.nextInt();
+        System.out.println("How many aces did they have?");
+        aces = s.nextInt();
+        System.out.println("How many assists/sets did they have?");
+        sets = s.nextInt();        
+        System.out.println("How many digs/passes did they have?");
+        digs = s.nextInt();
+        
+        System.out.println("How many net violations did they have?");       
+        netViolation = s.nextInt();
+        System.out.println("How many foot faults did they have?");
+        footFault  = s.nextInt();
+        System.out.println("How many injuries did they have?");
+        injuries = s.nextInt();
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
+
+    public String toString()
     {
-        // put your code here
-        return x + y;
+        String vballThings;
+        
+        vballThings = "Personal Stats:\n Height:  " + height+ "\n Reach:  " +reach+ "\n Verticle:  "+ verticle+ "\n Block Jump:  " +blockJump + "\n Approach Jump:  " + approachJump;
+        vballThings+="\n\n Game Stats:\n Blocks:  "+blocks+ "\n Hits:  "+hits+ "\n Kills:  "+ kills+ "\n Aces:  "+aces+ "\n Assists/Sets:  "+sets+ "\n Digs/Passes:  "+digs;
+        vballThings+= "\n\n Penalty Stats:\n Net Violations:  " + netViolation+ "\n Foot Faults:  "+ footFault+ "\n Injuries:  "+ injuries;
+        return vballThings;
     }
 }
